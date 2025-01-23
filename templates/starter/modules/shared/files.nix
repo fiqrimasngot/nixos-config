245033f7301fc515d2dd5ae3251f38959a75fbad
Fiqri Masngot <fiqrim@Fiqris-MacBook-Pro.local>
2025-01-23 19:50:01 +0800

    add env

--- templates/starter/modules/shared/files.nix
+++ templates/starter/modules/shared/files.nix
@@ -1,6 +1,17 @@
 { pkgs, config, ... }:
 
 {
+  home.file.".zsh_history" = {
+    source = "./zsh_history";
+  };  
+  home.file.".ssh" = {
+    source = "./ssh";
+    recursive = true;
+  };
+  home.file."./.config/nvim" = {
+    source = "./config/nvim";
+    recursive = true;
+  };
   # Initializes Emacs with org-mode so we can tangle the main config
   ".emacs.d/init.el" = {
     text = builtins.readFile ../shared/config/emacs/init.el;
